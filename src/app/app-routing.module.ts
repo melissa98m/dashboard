c55f9604f7a66e4cdfad87d40f85fa98001ca10c
melissa98m <melissa.mangione@gmail.com>
2021-08-02 09:57:25 +0200

    FourOhFourCoponent and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomePageComponent } from './home-page/home-page.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+
+  { path: 'not-found' , component: FourOhFourComponent},
+  { path: '**' , redirectTo: 'not-found' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
